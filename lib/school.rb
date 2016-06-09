57e37e7c1152ff4308e86072d460042e1f3a98cd
greatermeans <gakouri@gmail.com>
2016-06-09 17:51:21 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name=nil,grade=nil)
+    @name = name
+    @grade = grade
+    @roster[@grade] ||= []
+    @roster[@grade] << @name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster.map {|grade,grade_array| grade_array.sort! }
+    roster.sort.to_h
+  end
+
+  def roster
+    @roster
+  end
+
+end 
